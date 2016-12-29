64f47333fccefb3a4127688d6ff7d0c067fcfa72
Chen896 <602129398@qq.com>
2016-12-29 14:55:25 +0800

    Index Model

---
+++ application/index/common.php
@@ -0,0 +1,18 @@
+<?php
+// Index模块的公共函数2016-12-29 10:51
+
+/**
+ * 获取分类名称
+ * @param  string  $path    文章分类cid
+ * @return string           分类名称
+ */
+function getCategoryName($path){
+    if($pos = strrpos($path, '-')){
+        $cid = substr($path, $pos+1);
+    }else{
+        $cid = $path;
+    }
+
+    $res = model('AddonCategory')->get($cid);
+    return $res->name;
+}
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,14 +5,42 @@ use app\common\controller\Home;
 class Index extends Home
 {
 
-    public function index($id){
+    public function index($id = 1){  // url 中需要定义 id
         # $sid = input('get.id');  # url()函数路由静态化，不能用 input() 获取$_GET参数2016-12-28
 
-        $data = array();
-        $this->assign($data);
+        $prefix = config('database.prefix');
+        $path = db('Addon_category')->query(" SELECT CONCAT(`path`, '-', `id`) pp FROM {$prefix}addon_category WHERE `id`=$id ");
+        $path = $path[0]['pp'];
+
+        # 文章列表
+        $where['cid'] = ['like', $path.'%'];  //字符开头
+        $list = db('AddonContent')->where($where)->order('id','desc')->paginate(20);
+
+        # 相关标签
+        $label = db('AddonCategory')->where('path', $path)->select();
+        if(!$label){
+            $pos = strrpos($path, '-');
+            $path = substr($path, 0, $pos);
+
+            $label = db('AddonCategory')->where('path', $path)->select();
+        }
+
+        $this->assign(array('list'=>$list, 'label'=>$label));
         $this->setMeta("技术文章");
 
         return $this->fetch();
     }
 
+    public function detail($id){
+        $article = model('AddonContent')->getArticle($id);
+
+        //相关文章
+        $cid = $article['cid'];
+        $list = db('AddonContent')->field('id,title')->where('cid', $cid)->limit(15)->select();
+
+        $this->assign(array('article'=>$article, 'list'=>$list, 'id'=>$id));
+        $this->setMeta("文章详情");
+
+        return $this->fetch();
+    }
 }
